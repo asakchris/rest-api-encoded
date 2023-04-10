6c628eb67b3ff254e3bfde6c664a2a843a2dd8f6
Kamaraj Christopher <k.appadurai@spglobal.com>
2023-04-10 10:16:32 -0500

    Log all request headers

--- server/src/main/java/org/example/server/controller/UserController.java
+++ server/src/main/java/org/example/server/controller/UserController.java
@@ -1,12 +1,15 @@
 package org.example.server.controller;
 
 import java.util.List;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.server.entity.User;
 import org.example.server.service.UserService;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,8 +21,12 @@ public class UserController {
 
   @GetMapping(value = "/users")
   @ResponseStatus(code = HttpStatus.OK)
-  public List<User> findAllUsers() {
+  public List<User> findAllUsers(@RequestHeader MultiValueMap<String, String> headers) {
     log.info("Enter findAllUsers");
+    headers.forEach((key, value) -> {
+      log.info(String.format(
+          "Header '%s' = %s", key, value.stream().collect(Collectors.joining("|"))));
+    });
     return service.findAllUsers();
   }
 }
